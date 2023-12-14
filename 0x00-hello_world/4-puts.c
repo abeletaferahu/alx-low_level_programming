f195fdb5c2b375bdafe1055c795bdb6b512cc94d
abeletaferahu <abeletaferahu@gmail.com>
2023-12-14 11:29:27 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main aprogram that print a lin
- * return
+ * main aprogram that print a line puts function
+ * return 0 (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
